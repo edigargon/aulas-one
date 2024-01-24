52fd3f159bba614da92d9520810c59ca6b0d7cf0
Edigar Gonçalves da Silva <edigargoncalvess@gmail.com>
2024-01-24 14:19:31 -0300

    Finalizado Curso Lógica de Programação
    - Adicionado o modulo 5
      - Modulo Desafio
    
    - Finalizado o Curso Lógica de Programação: Mergulhe em Programação com JavaScript

---
+++ Lógica de Programação - Mergulhe em Programação com JavaScript/05 - Desafio/logica-js-projeto_inicial/app.js
@@ -0,0 +1,24 @@
+alert('Boas Vindas ao Jogo do Número Secreto');
+let numeroMaximo = 1000
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+
+    if (numeroSecreto == chute) {
+        break;
+    } else {
+        if (numeroSecreto < chute) {
+            alert(`O número secreto é menor que o chute ${chute}`);
+        } else {
+            alert(`O número secreto é maior que o chute ${chute}`);
+        }
+        tentativas++;//adicionando +1 em tentativas
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; //Operador Ternario
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
