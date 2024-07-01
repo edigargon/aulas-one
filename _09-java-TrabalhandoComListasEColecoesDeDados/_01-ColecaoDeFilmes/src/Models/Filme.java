0227e63a6d74e2e58d250eeeffe3f5a176d5d41a
edigar <edigargoncalvess@gmail.com>
2024-07-01 16:44:57 -0300

    Conclusão do Modulo 1;
    - Conclusão do Modulo 1 do Curso Trabalhando com Listas e Coleções;

---
+++ _09-java-TrabalhandoComListasEColecoesDeDados/_01-ColecaoDeFilmes/src/Models/Filme.java
@@ -0,0 +1,26 @@
+package Models;
+
+import Calculadora.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
+
+    private String diretor;
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    @Override
+    public int getClassificao() {
+        return (int) (retornaMedia() / 2);
+    }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
+    }
+}
