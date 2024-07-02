13bb04d329ff6020c2c348828482e6cc56b888e4
edigar <edigargoncalvess@gmail.com>
2024-07-02 19:45:52 -0300

    Inicializao do Modulo 1 do Curso 10;
    - Curso: Consumindo API, Gravando Arquivos e Lidando com Erros;
    - Conclusão do Módulo 1 - Requisições e Respostas;

---
+++ _10-Java-ConsumindoAPIGravandoArquivosELidandoComErros/_01-RequisicoesERespostas/src/Principal/PrincipalComBusca.java
@@ -0,0 +1,26 @@
+package Principal;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
+public class PrincipalComBusca {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o filme que gsotaria de buscar: ");
+        String busca = scanner.nextLine();
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=7e88ab5d";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+    }
+}
